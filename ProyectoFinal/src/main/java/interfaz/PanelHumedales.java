f21a4cc0bf53c23ddc880f3865340e86893a23a4
jgodoy2022@ <jgodoy2022@udec.cl>
2023-11-30 02:37:52 -0300

    feat: Nuevas imagenes habitats

--- ProyectoFinal/src/main/java/interfaz/PanelHumedales.java
+++ ProyectoFinal/src/main/java/interfaz/PanelHumedales.java
@@ -7,9 +7,15 @@ public class PanelHumedales extends JPanel {
     private Alimento alimento;
     private Animales animales;
 
+    private ImageIcon fondo;
     public PanelHumedales() {
+        fondo = new ImageIcon("src/main/java/interfaz/Imagenes/panelhumedal.jpg");
 
         this.setPreferredSize(new Dimension(1000, 800));
-        this.setBackground(Color.BLACK);
+    }
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        // Dibuja la imagen de fondo
+        g.drawImage(fondo.getImage(), 0, 0, getWidth(), getHeight(), this);
     }
 }
